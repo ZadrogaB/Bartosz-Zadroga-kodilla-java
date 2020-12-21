6a3beac1c23bd63c2c9584d5ffbe69691b9bdfbd
ZadrogaB <zadroga.b@wp.pl>
2020-12-21 10:36:57 +0100

    7.5 Modified ArrayOperationsTestSuite. Now it works on particular values. Delete unnecessary code.

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,11 +1,7 @@
 package com.kodilla.stream.array;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,20 +10,15 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage(){
         //Given
-        Random random = new Random();
         int[] testArray = new int[10];
-        int sum =0;
         for (int i=0; i<10; i++){
-            testArray[i] = random.nextInt(100);
-            sum+= testArray[i];
+            testArray[i] = i;
         }
-        double expected = Double.valueOf(sum)/10.0;
-        System.out.println("Expected: " + expected);
+
         //When
         double result = ArrayOperations.getAverage(testArray);
-        System.out.println("Result" + result);
 
         //Then
-        assertEquals(expected, result, 0.01);
+        assertEquals(4.5, result, 0.01);
     }
 }
