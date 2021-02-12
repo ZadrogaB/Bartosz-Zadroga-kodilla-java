0548957557fe0cd8f9037a632400c5c447e3b07e
ZadrogaB <zadroga.b@wp.pl>
2021-02-12 13:56:16 +0100

    14.3 Exercise and work with module

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -3,6 +3,10 @@ package com.kodilla.spring.library;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class LibraryTestSuite {
@@ -11,7 +15,7 @@ class LibraryTestSuite {
     private Library library;
 
     @Test
-    public void testLoadFromDb() {
+     void testLoadFromDb() {
         //Given
         //When
         library.loadFromDb();
@@ -20,11 +24,25 @@ class LibraryTestSuite {
     }
 
     @Test
-    public void testSaveToDb() {
+     void testSaveToDb() {
         //Given
         //When
         library.saveToDb();
         //Then
         //do nothing
+    }
+
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
     }
 }
